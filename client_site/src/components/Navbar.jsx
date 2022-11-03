fde3b8bd3c73946afb5a820e24ca4bfed5458ba2
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-11-03 15:52:10 +0530

    Add Logo Image

--- client_site/src/components/Navbar.jsx
+++ client_site/src/components/Navbar.jsx
@@ -1,6 +1,21 @@
+import { HiMenuAlt4 } from "react-icons/hi";
+import { AiOutlineClose } from "react-icons/ai";
+import logo from "../../images/logo.png";
+
+const NavBarItem = ({ title, classprops }) => (
+    <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+);
+
 const Navbar = () => {
     return (
-      <h1> Navbar</h1>
+        <nav className="w-full flex md:justify-center justify-between items-center p-4">
+            <div className="md:flex-[0.9] flex-initial justify-center items-center">
+                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+            </div>
+            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+            <NavBarItem title="Home" classprops="text-white" />
+            </ul>
+        </nav>
     );
 }
 
