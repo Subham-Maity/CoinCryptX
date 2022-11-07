c8aac51dcb9c75b6fffcb806b5e33cc86743e796
Subham <97989643+Subham-Maity@users.noreply.github.com>
2022-11-07 16:56:00 +0530

    Mojor Change ⚠️♦️⁉️
    1. All dependencies update
    2. Readme Update and Navbar update
    3. Tailwind CSS Not Working Fixed

--- client_site/src/components/Navbar.jsx
+++ client_site/src/components/Navbar.jsx
@@ -3,20 +3,25 @@ import { AiOutlineClose } from "react-icons/ai";
 import logo from "../../images/logo.png";
 
 const NavBarItem = ({ title, classprops }) => (
-    <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
+  <li className={`mx-4 cursor-pointer ${classprops}`}>{title}</li>
 );
 
 const Navbar = () => {
-    return (
-        <nav className="w-full flex md:justify-center justify-between items-center p-4">
-            <div className="md:flex-[0.9] flex-initial justify-center items-center">
-                <img src={logo} alt="logo" className="w-32 cursor-pointer" />
-            </div>
-            <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
-            <NavBarItem title="Home" classprops="text-white" />
-            </ul>
-        </nav>
-    );
+  return (
+    <nav className="w-full flex md:justify-center justify-between items-center p-4">
+      <div className="md:flex-[0.9] flex-initial justify-center items-center">
+        <img src={logo} alt="logo" className="w-32 cursor-pointer" />
+      </div>
+      <ul className="text-white md:flex hidden list-none flex-row justify-between items-center flex-initial">
+        {["Market", "Exchange", "Tutorials", "Wallets"].map((item, index) => (
+          <NavBarItem key={item + index} title={item} />
+        ))}
+        <li className="bg-[#5a879b] py-2 px-7 mx-4 rounded-br-lg cursor-pointer hover:bg-[#2e454f]">
+          Login
+        </li>
+      </ul>
+    </nav>
+  );
 }
 
 export default Navbar;
